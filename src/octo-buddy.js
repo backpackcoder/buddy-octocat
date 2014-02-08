9420d7483dae563fafbbfb3b719cc5bedfc27250
backpackcoder <andrew.hammer@backpackcoder.com>
2014-02-08 13:39:33 -0500

    Added +1 counting functionality

--- src/octo-buddy.js
+++ src/octo-buddy.js
@@ -1,35 +1,106 @@
 $(function(){
-    var base_url="https://api.github.com/repos/vs-networks/vsn/pulls";
-    var pull_template = $('#pull_template').text();
+    var base_url="https://api.github.com/repos/" + localStorage["repo"] + "/pulls";
+    var pull_template = $("#pull_template").text();
 
     var token = localStorage["token"];
     var user = localStorage["user"];
+    var repo = localStorage["repo"];
 
-    if (token && user) {
-        //$('#needs-config').addClass('hidden');
+    var $pulls = $("#pulls");
+    var $reviews = $("#reviews");
+
+    var regex = /@\S+/ig
+
+    function count_plus_ones(pull, last_commit_time) {
+        // Check for +1's
+        $.getJSON(pull.comments_url + "?access_token=" + token
+            , function(comments) {
+                for(var j in comments) {
+                    var ts = Date.parse(comments[j].updated_at);
+                    if (ts > last_commit_time) {
+                        if (comments[j].body.indexOf("+1") > -1 ||
+                            comments[j].body.indexOf(":b") > -1) {
+                            console.log(pull.number + " +1 " + comments[j].user.login);
+                            var img = $('#pull_' + pull.number + ' img[title="' + comments[j].user.login + '"]');
+                            var canvas = $('<canvas width="20px" height="20px"></canvas>');
+                            //console.log(img);
+                            if (img.length) {
+                                var ctx = canvas[0].getContext("2d");
+                                ctx.font = "bold 16px Arial";
+                                ctx.fillStyle = "#0E410E";
+                                ctx.globalAlpha = 0.5;
+                                ctx.drawImage(img[0], 0, 0, 20, 20);
+                                ctx.globalAlpha = 1.0;
+                                ctx.fillText("+1", 0, 16);
+                                canvas.attr("title", img.attr("title"));
+                                img.replaceWith(canvas);
+                            }
+                        }
+                    }
+                }
+            });
+    }
+
+    function calc_plus_ones(pull) {
+        // Get last commit time
+        $.getJSON(pull.commits_url + "?access_token=" + token
+            ,function(commits){
+                var last_commit_time = 0;
+                for(var i in commits) {
+                    var date_str = commits[i].commit.committer.date;
+                    last_commit_time = Math.max(last_commit_time, Date.parse(date_str));
+                }
+                count_plus_ones(pull, last_commit_time)
+            });
+    }
+
+
+    if (token && user && repo) {
         $.getJSON(base_url + "?access_token=" + token)
             .done(function(data){
                 for(var i in data) {
                     var pull = data[i];
-                    var $el = $( Mustache.render(pull_template, data[i]));
-                    if (user === pull.user.login) {
-                        $('#my-pulls').append($el);
-                        $('#my-pulls').prev('h2').removeClass('hidden');
+
+                    pull.ext = {
+                        width : 300,
+                        reviewers: []
                     }
                     var lines = pull.body.split('\n');
                     for(var j in lines) {
                         var line = lines[j].toLowerCase();
                         if (line.indexOf('cc') < 0 &&
-                            line.indexOf('@' + user) > 0) {
-                            $("#my-reviews").append($el);
-                            $("#my-reviews").prev('h2').removeClass('hidden');
+                            line.indexOf('@') > -1) {
+                            var matched = line.match(regex);
+                            for (var k in matched) {
+                                var login = matched[k].replace(/[@,]/g,'');
+                                pull.ext.width -= 24;
+                                pull.ext.reviewers.push({
+                                    login: login,
+                                    avatar_url: localStorage["cache.user." + login]
+                                });
+                            }
                         }
                     }
+
+                    console.log(pull);
+                    var $el = $(Mustache.render(pull_template, data[i]));
+                    if (user === pull.user.login) {
+                        $pulls.append($el);
+                        $pulls.prev('h2').removeClass('hidden');
+                    }
+                    for(var j in lines) {
+                        var line = lines[j].toLowerCase();
+                        if (line.indexOf('cc') < 0 &&
+                            line.indexOf('@' + user) > -1) {
+                            $reviews.append($el);
+                            $reviews.prev('h2').removeClass('hidden');
+                        }
+                    }
+                    calc_plus_ones(pull);
                 }
             })
             .fail(function(data){
-                $('#needs-config').text(data).removeClass("hidden");
-
+                console.log("ERROR", data);
             });
     } else {
         $('#needs-config').removeClass('hidden');
--- src/options.js
+++ src/options.js
@@ -2,7 +2,8 @@
 function save_options() {
   localStorage["token"] = $("#token").val();
   localStorage["user"] = $("#user").val();
-  $('#status').removeClass("hidden")
+  localStorage["repo"] = $("#repo").val();
+  $('#status').removeClass("hidden");
   setTimeout(function() { $('#status').addClass("hidden"); }, 750);
 }
 
@@ -10,12 +11,93 @@ function save_options() {
 function restore_options() {
     $("#token").val( localStorage["token"] );
     $("#user").val( localStorage["user"] );
+    load_repos();
 }
+
+
+function update_user_img(name, url) {
+    localStorage["cache.user." + name] = url;
+}
+
+function load_users(org) {
+    $.getJSON("https://api.github.com/orgs/" + org + "/members?access_token=" + $("#token").val(),
+        function(members) {
+            for(var i in members) {
+                update_user_img(members[i].login, members[i].avatar_url);
+            }
+        });
+}
+
+var repo_list = [], old_cursor = null;
+function load_repos() {
+    repo_list = [];
+    old_cursor = $('#repo').css("cursor");
+    $('#repo')
+        .empty()
+        .append($('<option>loading...</option>'))
+        .prop("disabled", true)
+        .css('cursor', 'wait');
+    $.getJSON("https://api.github.com/user?access_token=" + $("#token").val(),
+        function(user){
+            $('#user').val(user.login);
+            var repos_loaded = 0, total_repos = 1;
+            function repo_load_complete() {
+                repos_loaded++;
+                if (repos_loaded == total_repos) {
+                    $('#repo')
+                        .empty()
+                        .prop("disabled", false)
+                        .css("cursor", old_cursor);
+                    for(var i=0; i < repo_list.length; i++){
+                        if (repo_list[i] == localStorage["repo"]) {
+                            $('#repo')
+                                .append($('<option selected="selected">' + repo_list[i] + '</option>'))
+                        } else {
+                            $('#repo')
+                                .append($('<option>' + repo_list[i] + '</option>'))
+                        }
+                    }
+                }
+            }
+
+            function load_repos(org) {
+                $.getJSON("https://api.github.com/orgs/" + org + "/repos?access_token=" + $("#token").val(),
+                    function(repos) {
+                        for(var i=0; i < repos.length; i++){
+                            repo_list.push(org + '/' + repos[i].name);
+                        }
+                        repo_load_complete();
+                });
+            }
+
+            $.getJSON("https://api.github.com/user/orgs?access_token=" + $("#token").val(),
+                function(orgs){
+                    total_repos += orgs.length;
+                    for(var i=0; i < orgs.length; i++) {
+                        load_repos(orgs[i].login);
+                        load_users(orgs[i].login);
+                    }
+            });
+
+            $.getJSON("https://api.github.com/user/repos?access_token=" + $("#token").val(),
+                function(repos){
+                    for(var i=0; i < repos.length; i++) {
+                        repo_list.push(user.login + '/' + repos[i].name);
+                    }
+                    repo_load_complete();
+                });
+        });
+};
 
 $(function(){
     restore_options();
+
     $('button').click(function(ev){
         ev.preventDefault();
         save_options();
     });
+
+    $('#token').change(function(){
+        load_repos();
+    })
 });
