60c8816470e5816d0c158bcf7c94dd62660a336c
backpackcoder <andrew.hammer@backpackcoder.com>
2014-03-10 01:44:49 -0400

    Added code to list issues with a list of sub issues.

--- src/buddy-octocat.js
+++ src/buddy-octocat.js
@@ -1,5 +1,6 @@
 $(function(){
     var base_url="https://api.github.com/repos/" + localStorage["repo"] + "/pulls";
+    var issue_base_url="https://api.github.com/issues";
     var pull_template = $("#pull_template").text();
 
     var token = localStorage["token"];
@@ -10,6 +11,9 @@ $(function(){
     var $reviews = $("#reviews");
 
     var regex = /@\S+/ig
+    var checkbox_re = /.*\[([x\s])\].*/i;
+    var issue_re = /\s+#(\d+)\s+/i;
+
 
     function count_plus_ones(pull, last_commit_time) {
         // Check for +1's
@@ -20,10 +24,8 @@ $(function(){
                     if (ts > last_commit_time) {
                         if (comments[j].body.indexOf("+1") > -1 ||
                             comments[j].body.indexOf(":b") > -1) {
-                            console.log(pull.number + " +1 " + comments[j].user.login);
                             var img = $('#pull_' + pull.number + ' img[title="' + comments[j].user.login + '"]');
                             var canvas = $('<canvas width="20px" height="20px"></canvas>');
-                            //console.log(img);
                             if (img.length) {
                                 var ctx = canvas[0].getContext("2d");
                                 ctx.font = "bold 16px Arial";
@@ -54,6 +56,64 @@ $(function(){
             });
     }
 
+    function load_issues() {
+        $.getJSON(issue_base_url + "?access_token=" + token)
+            .done(function(data){
+                for(var i in data) {
+                    var issue = data[i];
+                    var checked = 0;
+                    var unchecked = 0;
+                    var body_lines = issue.body.split('\n');
+                    for(var j in body_lines) {
+                        var line = body_lines[j];
+                        var matches = checkbox_re.exec(line);
+                        if (matches) {
+                            console.log(line, matches);
+                            if (matches[1] == 'x' || matches[1] == 'X') {
+                                checked++;
+                            } else {
+                                unchecked++;
+                            }
+                        }
+                    }
+
+                    if (checked != 0 || unchecked != 0) {
+                        console.log(body_lines);
+                        var progress_str = '<span>(' + checked + '/' + (checked + unchecked) + ')</span>';
+                        var $link = $('<a href="' + issue.html_url + '" target="_blank" class="pull">' + issue.title + ' ' + progress_str + '</a>');
+                        $('#issues').append($link);
+                    }
+                }
+            })
+            .fail(function(data){
+                console.log("ERROR", data);
+            });
+    }
+
+    /*
+
+checkbox_re = re.compile("^-\s\[([x\s])\]\s")
+issue_re = re.compile("\s+#(\d+)\s+")
+
+widget_issue = vsn.get_issue(4420)
+b = widget_issue.body
+blines = b.split('\n')
+
+for line in blines:
+    checked = False
+    issue_number = 0
+    cb_match = checkbox_re.search(line)
+    if cb_match:
+        checked = (cb_match.group(1) == 'x')
+        i_match = issue_re.search(line)
+        if i_match:
+            issue_number = i_match.group(1)
+        print checked, issue_number
+
+
+     */
+
+
 
     if (token && user && repo) {
         $.getJSON(base_url + "?access_token=" + token)
@@ -82,7 +142,7 @@ $(function(){
                         }
                     }
 
-                    console.log(pull);
+                    //console.log(pull);
                     var $el = $(Mustache.render(pull_template, data[i]));
                     if (user === pull.user.login) {
                         $pulls.append($el);
@@ -105,4 +165,5 @@ $(function(){
     } else {
         $('#needs-config').removeClass('hidden');
     }
+    load_issues();
 });
